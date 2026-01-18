852491ff9073cbb7d775675751955a1d8bd2328e
Manish Chhetri <chhetrim901@gmail.com>
2026-01-18 15:11:33 +1030

    Implemeted welcome email

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 import { generateToken } from "../lib/utils.js"
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import { ENV } from "../lib/env.js";
 
 export const signup = async (req, res) => {
     const {fullName, email, password} = req.body
@@ -32,8 +34,9 @@ export const signup = async (req, res) => {
         });
 
         if (newUser){
-           generateToken(newUser._id, res);
-           await newUser.save();
+
+           const savedUser = await newUser.save(); 
+           generateToken(savedUser._id, res);
            
            res.status(201).json({
             _id: newUser._id,
@@ -41,6 +44,17 @@ export const signup = async (req, res) => {
             email: newUser.email,
             profilePic: newUser.profilePic,
            });
+
+           try {
+            await sendWelcomeEmail(savedUser.email, savedUser.fullName, ENV.CLIENT_URL);
+
+
+           } catch (error) {
+            console.error("Failed to send welcome email:", error);
+
+           
+           }
+
 
         } else{
             res.status(400).json({message: "invalid user data"});
